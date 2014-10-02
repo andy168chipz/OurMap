320d977723f6669761806526d2da8b61a2b3e79f
andychou <andy168chipz@gmail.com>
2014-10-02 15:13:19 -0700

    Added image gallery

--- ourmap/handlers/base.py
+++ ourmap/handlers/base.py
@@ -2,10 +2,10 @@ __author__ = 'Andy'
 
 #
 import webapp2
-
+from google.appengine.api import users
 import jinja2
 import os
-
+import logging
 
 template_dir = os.path.join(os.path.dirname(__file__), '../templates')  #path to template_dir, __file__(dir of cur file)
 #set up jinja_env w/ template dir
@@ -33,4 +33,11 @@ class AboutHandler(Handler):
 
 class testingHandler(Handler):
 	def get(self):
-		self.render("index.html")
+		user = users.get_current_user()
+		if user:
+			greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                        (user.nickname(), users.create_logout_url('/')))
+		else:
+			greeting = ('<a href="%s">Sign in or register</a>.' %
+                        users.create_login_url('/'))
+		self.response.out.write("<html><body>%s</body></html>" % greeting)
