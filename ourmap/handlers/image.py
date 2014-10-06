3987da420c988a71c27bff05ef989196c886e3d8
Andy <Andy PC>
2014-10-05 20:00:11 -0700

    added flickr API, added page buttons. clicking on state now shows first 100 pictures. Page buttons doesn't work yet.

--- ourmap/handlers/image.py
+++ ourmap/handlers/image.py
@@ -1,7 +1,14 @@
 __author__ = 'andychou'
 
-from base import Handler
 import logging
+
+from base import Handler
+import json
+import urllib2
+import sys
+sys.path.append('../')
+from ourmap.flickr_keys import API_KEY, API_SECRETS,USER_ID
+from google.appengine.api import urlfetch
 
 
 
@@ -12,9 +19,59 @@ class imageHandler(Handler):
 		self.redirect("/")
 		pass
 	def post(self):
-		data = self.request.get('state')
-		logging.error(data)
-		#state = data.name
-		return self.write("success" + data)
-	def query_image(self, state):
-		pass
+		result={}
+		state = self.request.get('state')
+		page_wanted = self.request.get('cur_page')
+		logging.error(page_wanted=="")
+		json_image_list = query_image_list(str(state))
+		cur_page, pages = get_pages(json_image_list)
+		url_dict = create_photo_html(json_image_list)
+		result['url_dict'] = url_dict
+		result['pages_count'] = pages
+		result['page'] = cur_page
+		json_data = json.dumps(result)
+		return self.write(json_data)
+
+#this function queries a API request and convert it to json format
+def query_image_list(state):
+	url = photo_search_url(API_KEY, USER_ID, state)
+	return get_json(url)
+
+#thie function takes a json format of photo info
+#then converts it to a list of URLs
+def create_photo_html(json_image_list):
+	url =""
+	for i in json_image_list['photos']['photo']:
+		farmid = i['farm']
+		serverid = i['server']
+		id = i['id']
+		secret = i['secret']
+		title = i['title']
+		url += (create_a_html(str(farmid), str(serverid), str(id), str(secret), title))
+	return url
+
+#this function converts flickr photo info into URL format
+def create_a_html(farmid, serverid, id, secret,title):
+	small_pic ='https://farm'+farmid+'.staticflickr.com/'+serverid+'/'+id+'_'+secret+'_s.jpg'
+	imgsrc = 'https://farm'+farmid+'.staticflickr.com/'+serverid+'/'+id+'_'+secret+'_b.jpg'
+
+	a_link = '''<a href="%s" title="%s" data-gallery><img src="%s" alt="%s"></a>'''
+
+	return str(a_link % (imgsrc, title, small_pic, title))
+
+#returns how many pages given the Flickr json API response
+def get_pages(json_data):
+	pages = json_data['photos']['pages']
+	cur_page = json_data['photos']['page']
+	return (cur_page,pages)
+
+def photo_search_url(api_key,user_id,tags,pages='1',format='json&nojsoncallback=1'):
+	return 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key='+api_key+'&user_id='+user_id+'&tags='+tags+'&page='+pages+'&format='+format
+
+def get_json(url):
+	response = urlfetch.fetch(url)
+	json_data = json.loads(response.content)
+	return json_data
+
+
+
--- ourmap/templates/js/map.js
+++ ourmap/templates/js/map.js
@@ -1,5 +1,7 @@
 //http://newsignature.github.io/us-map/
 
+var cur_page;
+var statename;
 $(document).ready(function() {
 		 $('#map').usmap({
 
@@ -9,28 +11,71 @@ $(document).ready(function() {
 		//click function
 		click: function(event, data) {
 			$('#links').css('display', 'none');
-			 var statename = data.name;
-			 console.log(statename);
-			 //http://stackoverflow.com/questions/9509191/google-app-engine-with-ajax-request-and-python-interactions
+			 statename = data.name;
+			$loading = $('#loading');
+			$link = $('#links');
 			$.ajax({
 				url:"/image",
 				type:'POST',
-				data: { 'state':data.name},
+				data: { 'state':statename},
+				dataType: "json",
 				beforeSend: function() {
-    				$('#loading').html("<img src='/img/loading.gif' />");
+				    init();
+    				$loading.html("<img src='/img/loading.gif' />");
   				},
-				success: function(html){
-					//$('#loading').html("");
-					console.log(html);
-					$link = $('#links');
-					$link.show();
-
+				success: function(data){
+				    //success with no pics
+					$loading.html("");
+					if(data.url_dict ==""){
+					    init();
+					}
+					else{
+					//success with pics
+					    cur_page=data.page;
+					    console.log('cur_page='+cur_page);
+                        pages(data.pages_count);
+                        links(data.url_dict);
+                    }
 				},
 				error: function(html){
+				    $loading.hide();
+				    $('#error').show();
 					console.log('fail');
 				}
 			});
 		}
-		 });
+	});
+    //for dynamic content
+	$("#pages").on('click', 'button',function() {
+         console.log(this.value);
+         if(this.value != cur_page){
+
+         }
+
+    });
 
 });
+
+function pages(pages){
+    var buttons = "";
+    for(i =1; i <= pages; i++){
+            buttons += '<button id=page'+i+' type="button" class="btn btn-info " role="button" value='+i+'>'+i+'</button>';
+    }
+    $('#pages').html("");
+    $('#pages').append(buttons);
+    $('#pages').show();
+}
+
+function links(url_dict){
+    $link = $('#links');
+    $link.html("");
+    $link.append(url_dict);
+    $link.show();
+}
+
+function init(){
+    $link.html("");
+    $('#pages').html("");
+    $link.hide();
+    $('#pages').hide();
+}
